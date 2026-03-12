e9d3d44954d71171b7f3aeb1da0108facde7efd1
mohammed imran <mohammedimran2872@gmail.com>
2026-03-12 15:35:14 +0530

    first django project upload

---
+++ static/js/cart.js
@@ -0,0 +1,45 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+		
+        console.log('productId:',productId, 'Action:', action, )
+
+		console.log('User:', user)
+		const person = document.getElementById('user').textContent
+		if(user === 'AnonymousUser'){
+			console.log('not logged in')
+			
+           console.log(person) 
+
+			
+		}else{
+			updateUserOrder(productId, action)
+		}
+    })
+}
+	
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+	console.log('user can deooo====')
+		var url = 'update_item'
+		console.log('code user in url')
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken' :csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    console.log('Data:', data)
+			location.reload()
+		});
+}
